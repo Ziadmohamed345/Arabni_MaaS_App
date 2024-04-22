007c43c938cd439c2d70822a20fc029b108228bb
Ziad.Mohamed <ziadmohamed345@gmail.com>
2024-04-22 10:49:23 +0200

    Adding khaled changes

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:maasapp/screens/core/utils/string_constants.dart';
+import 'package:maasapp/screens/features/Register/views/screen/register.dart';
+
+void main() async {
+  Future.wait([ScreenUtil.ensureScreenSize()]).then((value) {
+    runApp(const MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      splitScreenMode: true,
+      ensureScreenSize: true,
+      designSize: const Size(375, 812),
+      builder: (context, child) {
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: StringConstants.appName,
+          home: RegisterScreen(),
+        );
+      },
+    );
+  }
+}
