fe0f7e9946ce7c56656a1208db266d6a0853cbe1
Ziad.Mohamed <ziadmohamed345@gmail.com>
2024-05-03 14:42:39 +0300

    Code Reorganization

--- lib/screens/core/utils/image_constants.dart
+++ lib/screens/core/utils/image_constants.dart
@@ -1,7 +1,6 @@
 abstract class ImageConstants {
   static const String profileImage = 'assets/images/Profileimage.png';
-  static const String appBarImage = 'assets/images/AppBarImage.png';
+  static const String appBarImage = 'assets/images/Arabni_Logo.png';
 
-  static const String illustrationImage =
-      "assets/images/Wav BuddiesAddress.png";
+  static const String illustrationImage = 'assets/images/LoginPic.png';
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:maasapp/screens/main.dart';
+import 'package:maasapp/screens/features/Register/views/screen/register.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MaterialApp(
-      home: HOME(),
+      home: RegisterScreen(),
     ));
 
     // Verify that our counter starts at 0.
