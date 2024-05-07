dc17a4cb4f1ea8e2a6206f40234f4f8289276399
abdelrhman237-a <120103889+abdelrhman237-a@users.noreply.github.com>
2024-05-07 22:13:19 +0300

    forget password, login,register done with some UI changes

--- lib/features/Register/views/screen/forgetPass.dart
+++ lib/features/Register/views/screen/forgetPass.dart
@@ -1,130 +1,237 @@
+// ignore_for_file: deprecated_member_use, use_build_context_synchronously
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class ForgetPasswordScreen extends StatefulWidget {
+  const ForgetPasswordScreen({super.key});
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<ForgetPasswordScreen> createState() => _ForgetPasswordScreenState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _mobileController = TextEditingController();
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
+  late final TextEditingController _oldPassword;
+  late final TextEditingController _newPassword;
+  late final TextEditingController _confirmPassword;
 
-  // Regular expression pattern for Egyptian mobile numbers
-  static final RegExp _egyptMobileRegex = RegExp(r'^(01[0-2]|015)[0-9]{8}$');
 
-  // Function to handle sending verification code
-  Future<void> _sendVerificationCode(
-      BuildContext context, String mobile) async {
-    if (!_egyptMobileRegex.hasMatch(mobile)) {
-      // Show error message if the mobile number is not valid
-      showDialog(
-        context: context,
-        builder: (BuildContext dialogContext) {
-          return AlertDialog(
-            title: const Text("Error"),
-            content: const Text("Please enter a valid Egyptian mobile number."),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(dialogContext).pop();
-                },
-                child: const Text("OK"),
-              ),
-            ],
-          );
-        },
-      );
-      return;
-    }
-
-    try {
-      // Simulate sending a verification code via SMS
-      // In a real app, you would use a service like Twilio to send SMS
-      print('Sending verification code to $mobile');
-
-      // Show a success message or navigate to a confirmation page
-      showDialog(
-        context: context,
-        builder: (BuildContext dialogContext) {
-          return AlertDialog(
-            title: const Text("Verification Code Sent"),
-            content: const Text(
-              "A verification code has been sent to your mobile number.",
-            ),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(dialogContext).pop();
-                },
-                child: const Text("OK"),
-              ),
-            ],
-          );
-        },
-      );
-    } catch (error) {
-      // Handle errors, show error message to user
-      showDialog(
-        context: context,
-        builder: (BuildContext dialogContext) {
-          return AlertDialog(
-            title: const Text("Error"),
-            content: Text(error.toString()),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(dialogContext).pop();
-                },
-                child: const Text("OK"),
-              ),
-            ],
-          );
-        },
-      );
-    }
+  @override
+  void initState() {
+    _oldPassword = TextEditingController();
+    _newPassword = TextEditingController();
+    _confirmPassword = TextEditingController();
+    super.initState();
   }
 
-  // Function to validate and submit the login form
-  void _submitForm(BuildContext context) {
-    if (_formKey.currentState!.validate()) {
-      final mobile = _mobileController.text.trim();
-      // Call function to handle sending verification code
-      _sendVerificationCode(context, mobile);
-    }
+  @override
+  void dispose() {
+    _oldPassword.dispose();
+    _newPassword.dispose();
+    _confirmPassword.dispose();
+    super.dispose();
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),
+        backgroundColor: const Color.fromARGB(255, 4, 24, 50),
+        leading: IconButton(
+          icon: const Row(
             children: [
-              TextFormField(
-                controller: _mobileController,
-                decoration: const InputDecoration(labelText: 'Mobile Number'),
-                keyboardType: TextInputType.phone,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your mobile number.';
-                  } else if (!_egyptMobileRegex.hasMatch(value)) {
-                    return 'Please enter a valid Egyptian mobile number.';
-                  }
-                  return null;
-                },
+              Icon(Icons.arrow_back, color: Colors.white),
+              SizedBox(width: 4), // Adding space between icon and text
+            ],
+          ),
+          onPressed: () {
+            Navigator.of(context).pushNamedAndRemoveUntil('/login/', (route) => false);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: const Color.fromARGB(255, 4, 24, 50),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 1),
+              const Text(
+                'Arabni',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 238, 50, 53),
+                  fontSize: 52,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              const SizedBox(height: 16.0),
+              const Text(
+                'Enhancing Urban Mobility',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 249, 248, 248),
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Create New Password",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 249, 248, 248),
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _oldPassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Enter Old Password',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 0.5, vertical: 0.5), // Corrected padding values
+                ),
+                style: const TextStyle(color: Colors.black),
+              ),
+              const SizedBox(height: 12),
+              TextFormField(
+                controller: _newPassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Enter New Password',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 0.5, vertical: 0.5), // Corrected padding values
+                ),
+                style: const TextStyle(color: Colors.black),
+              ),
+              const SizedBox(height: 12),
+              TextFormField(
+                controller: _confirmPassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Confirm New Password',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 0.5, vertical: 0.5), // Corrected padding values
+                ),
+                style: const TextStyle(color: Colors.black),
+              ),
+              const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () => _submitForm(context),
-                child: const Text('Send Verification Code'),
+                onPressed: () async {
+                  final oldPassword = _oldPassword.text;
+                  final newPassword = _newPassword.text;
+                  final confirmPassword = _confirmPassword.text;
+
+                  if (newPassword == confirmPassword) {
+                    if (newPassword == oldPassword) {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Password Unchanged'),
+                            content: const Text('The new password is the same as the old password.'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(); // Close the dialog
+                                },
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      final auth = FirebaseAuth.instance;
+                      final currentUser = auth.currentUser;
+                      final database = FirebaseDatabase.instance.reference();
+
+                      if (currentUser != null) {
+                        final credential = EmailAuthProvider.credential(email: currentUser.email!, password: oldPassword);
+                        try {
+                          await auth.currentUser!.reauthenticateWithCredential(credential);
+                          DataSnapshot snapshot = (await database.child('users').child(currentUser.uid).child('password').once()) as DataSnapshot;
+                          final existingPassword = snapshot.value.toString();
+
+                          if (existingPassword == newPassword) {
+                            print('Password already used by another user.'); // Debug message
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Password Already Used'),
+                                  content: const Text('The new password is already in use.'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(); // Close the dialog
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          } else {
+                            await auth.currentUser!.updatePassword(newPassword);
+                            Navigator.of(context).pushNamedAndRemoveUntil('/login/', (route) => false);
+                          }
+                        } catch (e) {
+                          print('Error: $e'); // Debug message for reauthentication error
+                        }
+                      }
+                    }
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Passwords Mismatch'),
+                          content: const Text('The new password and confirm password do not match.'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop(); // Close the dialog
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Color.fromARGB(255, 253, 251, 251),
+                  backgroundColor: const Color.fromARGB(255, 238, 50, 53),
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                ),
+                child: const Text('Confirm'),
+              ),
+              const SizedBox(height: 0.5),
+              Image.asset(
+                'assets/images/LoginPic.jpg',
+                width: 200,
+                height: 200,
               ),
             ],
           ),
