8a93809de78fa4253d8571ef345b90d70fc72ba7
abdelrhman237-a <120103889+abdelrhman237-a@users.noreply.github.com>
2024-05-25 00:59:37 +0300

    added different modes with it;s calculations of time and distance

--- lib/features/Iternairy/view models/maps.dart
+++ lib/features/Iternairy/view models/maps.dart
@@ -19,6 +19,28 @@ class _MapScreenState extends State<MapScreen> {
   final end = TextEditingController();
   bool isVisible = false;
   List<LatLng> routpoints = [const LatLng(52.05884, -1.345583)];
+  String selectedMode = 'driving-car';
+  Map<String, String> modeNames = {
+    'driving-car': 'Car',
+    'cycling-regular': 'Bike',
+    'foot-walking': 'Walking'
+  };
+  Map<String, int> travelTimes = {
+    'driving-car': 0,
+    'cycling-regular': 0,
+    'foot-walking': 0
+  };
+  int? distance;
+  LatLng? startMarker;
+  LatLng? endMarker;
+
+   // Icons for each mode
+  Map<String, IconData> modeIcons = {
+    'driving-car': Icons.directions_car,
+    'cycling-regular': Icons.directions_bike,
+    'foot-walking': Icons.directions_walk,
+  };
+
 
   Future<void> _setCurrentLocation() async {
     bool serviceEnabled;
@@ -51,11 +73,10 @@ class _MapScreenState extends State<MapScreen> {
       return;
     }
 
-    // When we reach here, permissions are granted and we can access the position.
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
 
-    if (position.latitude != 0.0 && position.longitude != 0.0) {
+    if (position.latitude != 0 && position.longitude != 0) {
       try {
         List<Placemark> placemarks = await placemarkFromCoordinates(
             position.latitude, position.longitude);
@@ -94,7 +115,7 @@ class _MapScreenState extends State<MapScreen> {
         print('End coordinates: $endLat, $endLng');
 
         var url = Uri.parse(
-            'https://api.openrouteservice.org/v2/directions/driving-car'
+            'https://api.openrouteservice.org/v2/directions/$selectedMode'
             '?api_key=5b3ce3597851110001cf624824ee2084bbf44bb2b4e345cf2d72f072'
             '&start=$startLng,$startLat'
             '&end=$endLng,$endLat');
@@ -104,11 +125,17 @@ class _MapScreenState extends State<MapScreen> {
         if (response.statusCode == 200) {
           var data = jsonDecode(response.body);
           var coordinates = data['features'][0]['geometry']['coordinates'];
+          var duration = data['features'][0]['properties']['segments'][0]['duration'];
+          var dist = data['features'][0]['properties']['segments'][0]['distance'];
           setState(() {
             routpoints = coordinates
                 .map<LatLng>((coord) => LatLng(coord[1], coord[0]))
                 .toList();
+            travelTimes[selectedMode] = (duration / 60).round(); // convert seconds to minutes and round
+            distance = (dist / 1000).round(); // convert meters to kilometers and round
             isVisible = true;
+            startMarker = LatLng(startLat, startLng);
+            endMarker = LatLng(endLat, endLng);
           });
         } else {
           print('Failed to load route: ${response.reasonPhrase}');
@@ -132,57 +159,88 @@ class _MapScreenState extends State<MapScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Map Screen',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+          'Iternary Plan',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Color.fromARGB(255, 75, 2, 6)),
         ),
-        backgroundColor: Colors.grey[500],
+        backgroundColor: const Color.fromARGB(255, 203, 199, 199),
       ),
-      backgroundColor: Colors.grey[300],
+      backgroundColor: const Color.fromARGB(255, 203, 199, 199),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: myInput(
-                          controler: start, hint: 'Enter Starting point'),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.my_location),
-                      onPressed: _setCurrentLocation,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                myInput(controler: end, hint: 'Enter Ending point'),
-                const SizedBox(
-                  height: 15,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey[500],
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: myInput(
+                        controler: start, hint: 'Enter location'),
                   ),
-                  onPressed: _getRoute,
-                  child: const Text('Press'),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
-                  height: 500,
-                  width: 400,
-                  child: Visibility(
-                    visible: isVisible,
-                    child: FlutterMap(
-                      options: MapOptions(
-                        center: routpoints[0],
-                        zoom: 10,
-                      ),
+                  IconButton(
+                    icon: const Icon(Icons.my_location),
+                    onPressed: _setCurrentLocation,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              myInput(controler: end, hint: 'Enter destination'),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(234, 255, 255, 255),
+                    ),
+                    onPressed: _getRoute,
+                    child: const Text('Directions'),
+                  ),
+                  const SizedBox(width: 10),
+                  
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (var mode in modeNames.keys)
+                    DropdownButton<String>(
+                      value: selectedMode,
+                      items: modeNames.keys.map((String mode) {
+                        return DropdownMenuItem<String>(
+                          value: mode,
+                          child: Row(
+                            children: [
+                              Icon(modeIcons[mode]),
+                              SizedBox(width: 5),
+                              Text(modeNames[mode]!),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedMode = newValue!;
+                        });
+                        _getRoute();
+                      },
+                    ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Expanded(
+                child: Visibility(
+                  visible: isVisible,
+                  child: FlutterMap(
+                    options: MapOptions(
+                      center: routpoints[0],
+                      zoom: 10,
+                    ),
                       children: [
                         TileLayer(
                           urlTemplate:
@@ -197,13 +255,42 @@ class _MapScreenState extends State<MapScreen> {
                                 color: Colors.blue,
                                 strokeWidth: 9)
                           ],
-                        )
-                      ],
-                    ),
+                        ),
+                        if (startMarker != null && endMarker != null)
+                          MarkerLayer(
+                            markers: [
+                              Marker(
+                                width: 80.0,
+                                height: 80.0,
+                                point: endMarker!,
+                               child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.location_on),
+                          color: Colors.red,
+                          iconSize: 45,
+                                ),
+                            ),
+                          ],
+                        ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+              Visibility(
+                visible: isVisible,
+                child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: Column(
+                    children: [
+                      Text('Estimated time ${modeNames[selectedMode]}: ${travelTimes[selectedMode]!.toStringAsFixed(2)} minutes'),
+                      Text('Distance: ${distance?.toStringAsFixed(2)} km'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
