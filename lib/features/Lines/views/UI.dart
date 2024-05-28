f5348e6c55c77a9aa66e237cf0aa7ffc0071bb68
Ziad.Mohamed <ziadmohamed345@gmail.com>
2024-05-28 12:16:11 +0300

    addding ui in lines section

---
+++ lib/features/Lines/views/UI.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:maasapp/core/utils/image_constants.dart';
+import 'package:maasapp/core/utils/string_constants.dart';
